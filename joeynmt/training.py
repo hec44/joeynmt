1e8b60e1dd3f31a0f57f9ecf162f8d9f48d607b7
hec44 <hectormb.9@gmail.com>
2020-09-26 13:37:39 +0200

    smallgcn

--- joeynmt/training.py
+++ joeynmt/training.py
@@ -363,7 +363,6 @@ class TrainManager:
                 # validate on the entire dev set
                 if self.steps % self.validation_freq == 0 and update:
                     valid_start_time = time.time()
-                    pdb.set_trace()
                     valid_score, valid_loss, valid_ppl, valid_sources, \
                         valid_sources_raw, valid_references, valid_hypotheses, \
                         valid_hypotheses_raw, valid_attention_scores = \
